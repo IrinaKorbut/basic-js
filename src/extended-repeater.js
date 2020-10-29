f3c75b6b2f720904f196812213a137ec0c5d239f
IrinaKorbut <irisharomka96@gmail.com>
2020-10-29 23:34:50 +0300

    implement task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -4,4 +4,54 @@ module.exports = function repeater(/* str, options */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+const CustomError = require("../extensions/custom-error");
+
+module.exports = function repeater(str, options) {
+  let result = '';
+  let separator = '+';
+  let additionSeparator = '|';
+  let tempArr = [];
+
+  if ('separator' in options) {
+    separator = options['separator'];
+  }
+
+  if ('addition' in options) {
+    let additionStr = '';
+    let tempArrAdd = [];
+    
+    if ('additionSeparator' in options) {
+      additionSeparator = options['additionSeparator'];
+    }
+
+    if ('additionRepeatTimes' in options) {
+      for (let j = 1; j <= options['additionRepeatTimes']; j++) {
+        if ( options['addition'] == null ) {
+          options['addition'] = 'null';
+        tempArrAdd.push(options['addition']);
+      }
+    }
+
+    additionStr = tempArrAdd.join(additionSeparator);
+    str += additionStr;
+  }
+
+  if ('repeatTimes' in options) {
+    for (let i = 1; i <= options['repeatTimes']; i++) {
+
+      if (str == null) {
+        str = 'null'        
+      }
+
+      tempArr.push(str)
+    }
+  }
+
+  if (!(options['repeatTimes'])) {     
+    return str;
+  }
+
+  result = tempArr.join(separator);
+  return result;
+};
   
